b1ec0e262a99a369a0c5042e5f98a9e7a104a4e7
empadaelanches <davivs31@gmail.com>
2024-12-18 10:53:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Configuração do banco de dados
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: '10.100.30.129',
+  user: 'root',
+  password: 'RPSaod42227',
+  database: 'empada_e_lanches'
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Rota para consulta
+app.post('/consulta', (req, res) => {
+  const query = req.body.query;
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send('Erro ao executar consulta: ' + err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando`);
+});
